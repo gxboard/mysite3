fb2df2972e542ead960e9100bcf35738199e6d2d
gxboard <gxboard@gmail.com>
2016-09-08 16:22:14 +0900

    메시지 컨버터 등록

--- mysite3/src/main/java/kr/co/saramin/mysite3/controller/IndexController.java
+++ mysite3/src/main/java/kr/co/saramin/mysite3/controller/IndexController.java
@@ -2,6 +2,7 @@ package kr.co.saramin.mysite3.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -16,5 +17,12 @@ public class IndexController {
 	public String main()
 	{
 	    return "main/index"; 
+	}
+	
+	@RequestMapping("hello")
+	@ResponseBody
+	public String hello()
+	{
+		return "hello, 안녕하세요? !@#$%^&*()_+|";
 	}
 }
