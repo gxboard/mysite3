aef754920d6830fec846e168467d2ade05f66423
gxboard <gxboard@gmail.com>
2016-09-10 14:13:02 +0900

    AOP

--- mysite3/src/main/java/kr/co/saramin/mysite3/dao/GuestbookDao.java
+++ mysite3/src/main/java/kr/co/saramin/mysite3/dao/GuestbookDao.java
@@ -12,6 +12,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import kr.co.saramin.mysite3.exception.UserDaoException;
 import kr.co.saramin.mysite3.vo.GuestbookVo;
@@ -41,7 +42,12 @@ public class GuestbookDao {
 	}
 	
 	public List<GuestbookVo> getList() {
+	    
+	    
 		List<GuestbookVo> list = sqlSession.selectList("guestbook.getList");
+		
+		
+		
 		return list;
 	}
 	
